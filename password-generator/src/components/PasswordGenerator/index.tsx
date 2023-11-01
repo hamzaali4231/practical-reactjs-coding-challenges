21306de644e78df8f683e0dfd0d5943140822b08
hamzaali4231 <u1763290@unimail.hud.ac.uk>
2023-11-01 17:31:08 +0000

    prettify and none checkbox select as well

--- password-generator/src/components/PasswordGenerator/index.tsx
+++ password-generator/src/components/PasswordGenerator/index.tsx
@@ -1,208 +1,191 @@
-import { exit } from 'process'
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
-import {  useEffect, useState, useTransition } from 'react'
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import { useEffect, useState } from 'react';
 
-import passwordGif from '../../assets/gif/password.gif'
-import { ReactComponent as Copy } from '../../assets/icons/copy.svg'
-import { ReactComponent as Refresh } from '../../assets/icons/refresh.svg'
-import Checkbox from '../Checkbox'
-import './index.css'
+import passwordGif from '../../assets/gif/password.gif';
+import { ReactComponent as Copy } from '../../assets/icons/copy.svg';
+import { ReactComponent as Refresh } from '../../assets/icons/refresh.svg';
+import Checkbox from '../Checkbox';
+import './index.css';
 
 const checkboxList = [
-
-  {
-    id:0,
-    label:"upperCase",
-    name: "upper",
-    checked:false
-
-  },
-  {
-    id:1,
-    label:"lowerCase",
-    name: "lower",
-    checked:false
-
-  },
-  {
-    id:2,
-    label:"numbercase",
-    name: "number",
-    checked:false
-
-  },
-  {
-    id:3,
-    label:"specialCase",
-    name: "symbols",
-    checked:false
-
-  }
-]
-
-
+    {
+        id: 0,
+        label: 'upperCase',
+        name: 'upper',
+        checked: false
+    },
+    {
+        id: 1,
+        label: 'lowerCase',
+        name: 'lower',
+        checked: false
+    },
+    {
+        id: 2,
+        label: 'numbercase',
+        name: 'number',
+        checked: false
+    },
+    {
+        id: 3,
+        label: 'specialCase',
+        name: 'symbols',
+        checked: false
+    }
+];
 
 const PasswordGenerator = () => {
-const [passwordLength, setPasswordLength] = useState<number>(8)
-const [password, setPassword] =  useState<string>('')
+    const [passwordLength, setPasswordLength] = useState<number>(8);
+    const [password, setPassword] = useState<string>('');
 
-const upperCaseSelect = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const lowerCaseSelect = 'abcdefghijklmnopqrstuvwxyz'
-const numCaseSelect = '0123456789'
-const specCaseSelect = '!@#$%^&*()'
-const [data, setData] = useState( checkboxList)
-const [strength, setStrength] = useState()
-const [length, setLength] = useState<string>('WEAK')
+    const upperCaseSelect = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseSelect = 'abcdefghijklmnopqrstuvwxyz';
+    const numCaseSelect = '0123456789';
+    const specCaseSelect = '!@#$%^&*()';
+    const [data, setData] = useState(checkboxList);
+    const [length, setLength] = useState<string>('WEAK');
 
+    useEffect(() => {
+        let strength = 0;
 
-useEffect(()=>{
-let strength =0
-  
-  if(password.match(/[a-z]+/)){
-    strength+=1
-  }
-  else if(password.match(/[A-Z]+/)){
-    strength+=1
-  }
-  if(password.match(/[0-9]+/)){
-    strength+=1
-  }
-  if(password.match(/[!@#$%^&*()]+/)){
-    strength+=1
-  }
-  if(password.length<8){
-    strength=0
-  }
-  
-  switch(strength){
-    case 0:
-      setLength('Too Short')
-      break;
-    case 1:
-      setLength('Weak')
-      break;
-    case 2:
-      setLength('Medium')
-      break;
-    case 3:
-      setLength('Hard')
-      break;
-    case 4:
-      setLength('Too Short')
-      break;  
-  }
- 
-},[length, password])
-  const onChangePasswordLength = (value: number | number[]) => {
-    setPasswordLength(value as number)
-  }
+        if (password.match(/[a-z]+/)) {
+            strength += 1;
+        } else if (password.match(/[A-Z]+/)) {
+            strength += 1;
+        }
+        if (password.match(/[0-9]+/)) {
+            strength += 1;
+        }
+        if (password.match(/[!@#$%^&*()]+/)) {
+            strength += 1;
+        }
+        if (password.length < 8) {
+            strength = 0;
+        }
 
+        switch (strength) {
+            case 0:
+                setLength('Too Short');
+                break;
+            case 1:
+                setLength('Weak');
+                break;
+            case 2:
+                setLength('Medium');
+                break;
+            case 3:
+                setLength('Hard');
+                break;
+            case 4:
+                setLength('Too Short');
+                break;
+        }
+    }, [length, password]);
 
+    const handleAllRemovedChecked = () => {};
 
-  const copyPass = () => {
+    useEffect(() => {
+        if (data.every((item) => item.checked === false)) {
+            const updatedData = data.map((checkbox) =>
+                checkbox.label === 'lowerCase' ? { ...checkbox, checked: true } : checkbox
+            );
 
-    //map through the data array to check if 
+            setData(updatedData);
+        }
+    }, [data]);
 
-      console.log(password)
-  }
+    const onChangePasswordLength = (value: number | number[]) => {
+        setPasswordLength(value as number);
+    };
 
-  const handleChange = (id:number) => {
-    const updateCheckboxes = data.map((checkbox) => 
-    checkbox.id === id ? {...checkbox, checked: !checkbox.checked}:checkbox)
+    const handleChange = (id: number) => {
+        const updateCheckboxes = data.map((checkbox) =>
+            checkbox.id === id ? { ...checkbox, checked: !checkbox.checked } : checkbox
+        );
 
-    setData(updateCheckboxes)
+        setData(updateCheckboxes);
+    };
 
-  }
+    const createPass = () => {
+        let result: string = '';
 
-  const createPass = () => {
-    let result:string = ''
+        let selection: string[] = [];
 
-    let selection : string[] = []
+        data.map((item) => (item.checked === true ? selection.push(item.name) : ''));
+        for (let i = 0; i < selection.length; i++) {
+            if (selection[i] === 'upper') result += upperCaseSelect;
 
-    data.map((item) =>
-          item.checked === true ? selection.push(item.name) : ''
-    )
-    for (let i=0; i<selection.length ; i++) {
-        if(selection[i] ==='upper')
-        result += upperCaseSelect
+            if (selection[i] === 'lower') result += lowerCaseSelect;
 
-        if(selection[i] ==='lower')
-        result += lowerCaseSelect
+            if (selection[i] === 'number') result += numCaseSelect;
 
-        if(selection[i] ==='number')
-        result += numCaseSelect
+            if (selection[i] === 'symbols') result += specCaseSelect;
+        }
+        let tempPass = '';
 
-        if(selection[i] ==='symbols')
-        result += specCaseSelect
+        for (let i = 0; i < passwordLength; i++) {
+            tempPass += result.charAt(Math.floor(Math.random() * result.length));
+        }
 
-    }
-    let tempPass = ''
+        setPassword(tempPass);
+    };
 
-
-    for ( let i = 0; i < passwordLength; i++ ) {
-      tempPass += result.charAt(Math.floor(Math.random() * result.length));
-   }    
-   
-
-
-setPassword(tempPass)
-  }
-
-  return (
-    <div className="password-wrapper">
-      <div className="gif">
-        <img src={passwordGif} alt="Password Gif" />
-      </div>
-      <div className="tac">
-        <h2 className="title">PASSWORD GENERATOR</h2>
-        <p className="subtitle">
-          Create strong and secure passwords to keep your account safe online.
-        </p>
-      </div>
-      <div className="password-input-wrapper">
-        <div className="password-field">
-          <input type="text" placeholder="your password" value={password} />
-          <Refresh onClick={createPass} />
+    return (
+        <div className="password-wrapper">
+            <div className="gif">
+                <img src={passwordGif} alt="Password Gif" />
+            </div>
+            <div className="tac">
+                <h2 className="title">PASSWORD GENERATOR</h2>
+                <p className="subtitle">
+                    Create strong and secure passwords to keep your account safe online.
+                </p>
+            </div>
+            <div className="password-input-wrapper">
+                <div className="password-field">
+                    <input type="text" placeholder="your password" value={password} />
+                    <Refresh onClick={createPass} />
+                </div>
+                <button
+                    onClick={() => navigator.clipboard.writeText(password)}
+                    className="copy-btn"
+                >
+                    <Copy /> Copy
+                </button>
+            </div>
+            <span className="fw-500">{length}</span>
+            <div className="slider">
+                <div>
+                    <label id="slider-label">Password Length: </label>
+                    <span>{passwordLength}</span>
+                </div>
+                <Slider
+                    max={30}
+                    min={5}
+                    value={passwordLength}
+                    onChange={onChangePasswordLength}
+                    className="slider-style"
+                />
+            </div>
+            <div className="elements">
+                {data.map((obj) => {
+                    return (
+                        <div key={obj.id}>
+                            <Checkbox
+                                multiple={true}
+                                id={obj.id}
+                                label={obj.label}
+                                checked={obj.checked}
+                                name={obj.name}
+                                onChange={() => handleChange(obj.id)}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-        <button onClick={()=>navigator.clipboard.writeText(password)} className="copy-btn">
-          <Copy /> Copy
-        </button>
-      </div>
-      <span className="fw-500">{length}</span>
-      <div className="slider">
-        <div>
-          <label id="slider-label">Password Length: </label>
-          <span>{passwordLength}</span>
-        </div>
-        <Slider
-          max={30}
-          min={5}
-          value={passwordLength}
-          onChange={onChangePasswordLength}
-          className="slider-style"
-        />
-      </div>
-      <div  className="elements">
-        {data.map((obj) => {
+    );
+};
 
-          return(
-            <div key={obj.id}>
-          <Checkbox
-          multiple={true}
-          id={obj.id}
-          label = {obj.label}
-          checked= {obj.checked}
-          name= {obj.name}
-          onChange = {() => handleChange(obj.id)}
-          />
-                    </div>
-
-          )
-        })}
-        </div>
-        </div>
-  )
-}
-
-export default PasswordGenerator
+export default PasswordGenerator;
